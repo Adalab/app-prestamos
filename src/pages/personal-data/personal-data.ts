89ea7f8731114c49dfe6130667a7043be23a9c5b
Cristina Fernández Rodríguez <cristinafernandezrodriguez@MacBook-Pro-de-Cristina-2.local>
2017-06-07 13:49:30 +0200

    create a new folder

---
+++ src/pages/personal-data/personal-data.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-personal-data',
+  templateUrl: 'personal-data.html'
+})
+export class PersonalDataPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
