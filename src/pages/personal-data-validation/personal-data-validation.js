4de3a7224945c01575c1068ce38294cd7faf30ab
Cristina Fernández Rodríguez <cristinafernandezrodriguez@MacBook-Pro-de-Cristina-2.local>
2017-06-12 11:16:01 +0200

    new file js

---
+++ src/pages/personal-data-validation/personal-data-validation.js
@@ -0,0 +1,13 @@
+// 
+// var open = document.querySelector('.open-modal');
+// var modal = document.querySelector('.container-modal');
+// var close = document.querySelector('.close-modal');
+//
+// function openModal(){
+//   modal.style.display="block";
+// }
+// open.addEventListener('click', openModal);
+//
+// close.onclick=function() {
+// modal.style.display="none";
+// }
