23571c8281257d8fad382178f661eaaac584728e
Cristina Fernández Rodríguez <cristinafernandezrodriguez@MacBook-Pro-de-Cristina-2.local>
2017-06-12 22:55:11 +0200

    create a modal

--- src/pages/personal-data-validation/personal-data-validation.ts
+++ src/pages/personal-data-validation/personal-data-validation.ts
@@ -15,3 +15,19 @@ export class PersonalDataValidationPage {
     this.navCtrl.push(FarewellPage)
   }
 }
+
+//modal
+
+function modal() {
+$scope.openModal = function(){
+    $scope.modal.show();
+};
+
+$scope.closeModal = function(){
+    $scope.modal.hide();
+};
+
+$scope.$on('$destroy', function() {
+    $scope.modal.remove();
+});
+}
